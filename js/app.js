48872e2083f96513f66e5ac003a9623587fd3e41
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-06-01 23:05:08 -0500

    validación número de carácters, activado / desactivado

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
-$(document).ready(function() {
+(function(){
   
-  //Evento a formulario
-  $("#formulario").submit(function(e) {
+  // Botón que se utiliza en varias funciones
+  
+  
+  var cargarPagina = function() {
+    //Eventos
+  $("#formulario").submit(agregarTweet);
+  $("#tweet").keyup(validarNumeroDeCaracteres);
+    console.log(event);
+  };
+
+  var agregarTweet = function (e) {
     e.preventDefault();
     // Obtener elementos html
+    var $botonEnviar = $("#enviar");
     var $mensajeContenedor = $("#tweet");
     var $publicaciones = $("#publicar");
     var mensaje = $mensajeContenedor.val();
-    
+
     //Creamos elementos HTML desde el DOM
     var $contenedorPublicacion = $("<div class='jumbotron'/>");
-    
+
     //Agregamos elemento creado por DOM a elemento existente
     $contenedorPublicacion.text(mensaje);
     $publicaciones.prepend($contenedorPublicacion);
     
-  });
+    // Limpiar textArea
+    $mensajeContenedor.val(" ");
+    $botonEnviar.attr("disabled", true);
+  };
+
+  var validarNumeroDeCaracteres = function () {
+    var $botonEnviar = $("#enviar");
+    console.log($(this));
+    if ( $(this).val().trim().length > 0) {
+      $botonEnviar.removeAttr("disabled");
+    }
+  };
   
-});
+
+
+$(document).ready(cargarPagina);
+
+  })();
