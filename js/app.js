f1f0dc7c28be2786ae95e7922edd8174029859c5
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-06-05 01:52:58 -0500

    agregando fehcca y cambio de color al contadora al borrar caracteres

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@
     $("#tweet").keyup(validarNumeroDeCaracteres);
     $("#tweet").keyup(caracteresRestantes);
     $("#tweet").keyup(colorAlerta);
+    $("#tweet").keypress(cambiarTamanoTextArea);
   };
 
   var agregarTweet = function (e) {
@@ -20,14 +21,49 @@
     var $mensajeContenedor = $("#tweet");
     var $publicaciones = $("#publicar");
     var mensaje = $mensajeContenedor.val();
-
+    var date = new Date();
+    var dia = date.getDate();
+    var mes = date.getMonth() + 1;
+    var hora = date.getHours();
+    var ano = date.getFullYear();
+    var minutos =date.getMinutes();
+    var segundos = date.getSeconds();
+    
+    /* Ceros en formatos fecha dd:mm:aa y hora hh:mm:ss */
+    if(dia < 10) {
+      dia = "0" + date.getDate();
+    }
+    
+    if(mes < 10) {
+      mes = "0" + mes;
+    }
+    
+    if(hora < 10) {
+      hora = "0" + date.getHours();
+    }
+    
+    if(minutos < 10) {
+      minutos = "0" + date.getMinutes();
+    }
+    
+    if(segundos < 10) {
+      segundos = "0" + date.getSeconds();
+    }
     //Creamos elementos HTML desde el DOM
     var $contenedorPublicacion = $("<div class='jumbotron'/>");
     var $parrafo = $("<p />");
-
+    var $fecha = $("<span />");
+    var $hora=$(" "+"<span />");
+    
     //Agregamos elemento creado por DOM a elemento existente
     $parrafo.text(mensaje);
+    $fecha.text(dia + "/" + mes + "/" + ano, ', ');
+    $hora.text("  " + hora + ":"  + minutos + ":"+ segundos);
+    
+    
     $contenedorPublicacion.append($parrafo);
+    $contenedorPublicacion.append($fecha);
+    $contenedorPublicacion.append($hora);
     $publicaciones.prepend($contenedorPublicacion);
 
     // Limpiar textArea
@@ -65,12 +101,14 @@
   var colorAlerta = function() {
     var $imprContador = $("#alerta");
     if($(this).val().trim().length > 129) {
-      $imprContador.css("color", "red");
+      $imprContador.removeClass("naranja").addClass("rojo");
     } else if ($(this).val().trim().length > 119) {
-      $imprContador.css("color", "orange");
+      $imprContador.removeClass("rojo").addClass("naranja");
+    } else if ($(this).val().trim().length <= 119) {
+      $imprContador.removeClass("naranja");
     }
   };
-
+  
 
   $(document).ready(cargarPagina);
 
